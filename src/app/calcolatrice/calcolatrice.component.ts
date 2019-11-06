0ed9f6530f9f88e1a74cd377636704e1081b75aa
davideAlberti <davide.alberti@marconirovereto.it>
2019-11-06 22:42:45 +0100

    aggiunta la memoria

--- src/app/calcolatrice/calcolatrice.component.ts
+++ src/app/calcolatrice/calcolatrice.component.ts
@@ -7,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalcolatriceComponent {
   expression: string = '';
-  more: boolean = false;
+  memory: string[] = []
 
   constructor() { }
 
   aggiungi(carattere: string) {
-    var lastChar: string = this.expression.substr(this.expression.length - 1, this.expression.length)
+    var lastChar: string = this.expression.charAt(this.expression.length - 1)
 
     if(this.isOperator(lastChar) && this.isOperator(carattere)) {
       carattere = ''
@@ -24,10 +24,24 @@ export class CalcolatriceComponent {
   calcola() {
     var lastChar: string = this.expression.substr(this.expression.length - 1, this.expression.length)
 
-    if(this.isOperator(lastChar)) {
-      this.funzione('undo')
-      console.log('here')
+    if(this.isOperator(lastChar)) this.funzione('undo') 
+    if(this.expression.includes('sin(')) {
+      if(this.expression.includes('arc')) this.expression = this.expression.replace('arcsin(', 'Math.asin(')
+      else this.expression = this.expression.replace('sin(', 'Math.sin(')
     }
+    if(this.expression.includes('cos(')) {
+      if(this.expression.includes('arc')) this.expression = this.expression.replace('arcsin(', 'Math.acos(')
+      else this.expression = this.expression = this.expression.replace('cos(', 'Math.cos(')
+    }
+    if(this.expression.includes('tan(')) {
+      if(this.expression.includes('arc')) this.expression = this.expression.replace('arctan(', 'Math.atan(')
+      else this.expression = this.expression.replace('tan(', 'Math.tan(')
+    }
+    if(this.expression.includes('π')) this.expression = this.expression.replace('π', 'Math.PI')
+    if(this.expression.includes('√(')) this.expression = this.expression.replace('√(', 'Math.sqrt(')
+    if(this.expression.includes('log(')) this.expression = this.expression.replace('log(', 'Math.log10(')
+    if(this.expression.includes('10^(')) this.expression = this.expression.replace('10^(', `Math.pow(10, `)
+
     this.expression = eval(this.expression)
   }
 
@@ -41,11 +55,35 @@ export class CalcolatriceComponent {
         }
       }; break;
       case 'C': this.expression = ''; break;
-      case 'undo': this.expression = this.expression.substr(0, this.expression.length - 1); break;
+      case 'undo': this.expression = this.expression.slice(0, this.expression.length - 1); break;
     }
   }
 
   isOperator(char: string) {
     return char == '+' || char == '/' || char == '*' || char == '-' ? true : false;
+  }
+
+  session() {
+    this.memory.push(this.expression)
+    this.setSession(this.memory.toString())
+  }
+
+  get() {
+    if(!!sessionStorage.getItem('memory')) return sessionStorage.getItem('memory').split(',')
+  }
+
+  del(item) {
+    this.memory = this.get()
+    delete this.memory[this.memory.indexOf(item)]
+    this.setSession(this.memory)
+    return false
+  }
+
+  setSession(item) {
+    sessionStorage.setItem('memory', item)
+  }
+
+  clear() {
+    sessionStorage.removeItem('memory')
   }
 }
